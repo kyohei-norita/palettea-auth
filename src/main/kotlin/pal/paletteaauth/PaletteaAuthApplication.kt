eba1b13b4c37edaab4e159f90576f53025ffd877
norita <kyohei.norita@gmail.com>
2024-04-29 11:54:32 +0900

    first commit

---
+++ src/main/kotlin/pal/paletteaauth/PaletteaAuthApplication.kt
@@ -0,0 +1,11 @@
+package pal.paletteaauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PaletteaAuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<PaletteaAuthApplication>(*args)
+}
---
+++ src/test/kotlin/pal/paletteaauth/PaletteaAuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package pal.paletteaauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PaletteaAuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
